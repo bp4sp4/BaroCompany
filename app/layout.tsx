97cb921c01023b53c231b914357316c5670db293
박상훈 <bp4sp4@naver.com>
2025-12-24 11:47:40 +0900

    update og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "한평생 바로기업",
   description: "한평생 바로기업",
+  openGraph: {
+    title: "한평생 바로기업 | 정책자금 신청부터 기업성장 지원까지!",
+    description: "한평생 바로기업",
+    url: "https://barocompany.com",
+    siteName: "한평생 바로기업",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "한평생 바로기업",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "한평생 바로기업",
+    description: "한평생 바로기업",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
